71421aae64196d5e965bc9104a4c369e7bc852b2
Swastik Nath <swastik.nath@live.com>
2019-07-19 16:36:42 +0000

    A hello world flask application with database connection defined

---
+++ application.py
@@ -0,0 +1,31 @@
+# non-flask python imports
+import os
+import datetime
+
+# flask based and other python script imports
+from flask import Flask, flash, render_template, redirect, request, url_for, session
+from flask_session import Session
+from flask_bcrypt import generate_password_hash, check_password_hash
+from models import *
+
+app = Flask(__name__)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Check for environment variable DATABASE_URL
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app) # db as defined in models.py
+
+
+# Index page route
+@app.route('/')
+def index():
+    return "Hello DeltaX"
